7bf847d857cb25865825e03114067c583182d09c
Clark Angelo <clarklagumbay0@gmail.com>
2026-01-12 14:42:35 +0800

    Navigation sidebar design is done

---
+++ onlinequiz-frontend/src/components/teacher/TeacherNavigationBar.jsx
@@ -0,0 +1,101 @@
+import { FaBook, FaChartLine, FaFileExport, FaHome, FaUserFriends } from "react-icons/fa";
+
+export default function TeacherNavigationBar({ isSidebarOpen }) {
+
+    const navigationItems = [
+        {
+            label: 'Overview', items: [
+                { name: 'Dashboard', link: '/teacher-dashboard', icon: <FaHome /> },
+            ]
+        },
+        {
+            label: 'Quiz Management', items: [
+                { name: 'Quizzes', link: '/teacher-quizzes', icon: <FaBook /> },
+            ]
+        },
+        {
+            label: 'Class Management', items: [
+                { name: 'My Classes', link: '/teacher-students', icon: <FaUserFriends /> },
+            ]
+        },
+        {
+            label: 'Reports', items: [
+                { name: 'Results & Analytics', link: '/teacher-profile', icon: <FaChartLine /> },
+                { name: 'Reports & Export', link: '/teacher-settings', icon: <FaFileExport /> },
+            ]
+        },
+    ]
+
+    return (
+        <div className="">
+            <div className="p-2 h-14 mb-2 flex items-center border-b border-gray-700">
+                <div className="w-15 h-full bg-cover bg-no-repeat bg-center" style={{ backgroundImage: "url('/quiz-logo.png')" }}>
+                </div>
+                <h1 className={`font-bold text-md text-gray-300 tracking-wide ${!isSidebarOpen ? "hidden" : ""}`}>Quiz Time</h1>
+            </div>
+            <div className="">
+
+            </div>
+            <nav className={`${!isSidebarOpen ? "" : "px-2"}`}>
+                {navigationItems.map((section, index) => (
+                    <div key={index} className="mb-6">
+                        {!isSidebarOpen ? "" : (
+                            <h2 className="text-gray-300 uppercase text-sm mb-2">
+                                {section.label}
+                            </h2>
+                        )}
+                        <ul className="list-disc list-inside">
+                            {section.items.map((item, itemIndex) => (
+                                <ul key={itemIndex} className="">
+                                    <a
+                                        key={itemIndex}
+                                        href={item.link}
+                                        className={`
+                                    group relative
+                                    text-gray-400 hover:text-white
+                                    hover:bg-gray-600
+                                    flex items-center
+                                    p-2 rounded
+                                    ${isSidebarOpen ? "gap-2" : "justify-center"}
+                                `}
+                                    >
+                                        {/* ICON */}
+                                        <span className="text-2xl">
+                                            {item.icon}
+                                        </span>
+
+                                        {/* SECTION */}
+                                        {isSidebarOpen && (
+                                            <span>{item.name}</span>
+                                        )}
+
+                                        {/* TOOLTIP */}
+                                        {!isSidebarOpen && (
+                                            <span
+                                                className="
+                                            pointer-events-none
+                                            absolute left-full ml-3
+                                            top-1/2 -translate-y-1/2
+                                            whitespace-nowrap
+                                            bg-gray-900 text-white text-sm
+                                            px-3 py-1 rounded-md
+                                            opacity-0 group-hover:opacity-100
+                                            transition-opacity duration-200
+                                            shadow-lg
+                                            z-50
+                                        "
+                                            >
+                                                {item.name}
+                                            </span>
+                                        )}
+                                    </a>
+                                </ul>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+            </nav>
+        </div>
+    );
+
+}
---
+++ onlinequiz-frontend/src/components/teacher/contents/TeacherOverview.jsx
@@ -0,0 +1,22 @@
+export default function Overview() {
+    return (
+        <div className="bg-gray-200 h-full w-full">
+            <div className="grid md:grid-cols-4 gap-4 p-4 border">
+                <div className="h-36 bg-white p-4 rounded-lg shadow-md">
+
+                </div>
+                <div className="h-36 bg-white p-4 rounded-lg shadow-md">
+
+                </div>
+                <div className="h-36 bg-white p-4 rounded-lg shadow-md">
+
+                </div>
+                <div className="h-36 bg-white p-4 rounded-lg shadow-md">
+
+                </div>
+            </div>
+
+            
+        </div>
+    );
+}
